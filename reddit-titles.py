a72c48f2f1dbe11e8bb88aff2122b449120b84f6
smt <smtea923@gmail.com>
2016-05-11 20:49:08 +0100

    moved everything into functions, better input checking/handling

--- reddit-titles.py
+++ reddit-titles.py
@@ -3,32 +3,44 @@ import markovify
 import sys
 import requests
 
-inputgiven = False
-intitles = ""
 headers = {
     'User-Agent': 'python:reddit-title-generator/v0.1',
     'content-type': 'text'
 }
 
-# Check if the user has given input, if not, prompt them for it
-if len(sys.argv) == 1:
-    subreddit = input("Generate post title from which subreddit? (Just the name, no /r/): ")
-    url = requests.get("http://reddit.com/r/" + subreddit, headers=headers)
-elif len(sys.argv) > 2:
-    print("Please only enter a single argument, the subreddit name!")
-    exit()
-else:
-    url = requests.get("http://reddit.com/r/" + sys.argv[1], headers=headers)
 
-soup = BeautifulSoup(url.text, 'lxml')
+def crawl_url(input_url):
+    intitles = ""
+    soup = BeautifulSoup(input_url.text, 'lxml')
 
-# Find all the titles on a single reddit page
-titles = soup('a', {'class': 'title may-blank '})
+    # Find all the titles on a single reddit page
+    titles = soup('a', {'class': 'title may-blank '})
 
-# Get each title and make it more like a long paragraph (better for markovify)
-for title in titles:
-    intitles += title.text + '. '
+    # Get each title and make it more like a long paragraph (better for markovify)
+    for title in titles:
+        intitles += title.text + '. '
+    if intitles == '':
+        print("Crawl did not seem to work, please try a different subreddit!")
+        exit()
+    return intitles
 
-markov = markovify.Text(intitles, state_size=1)
 
-print(markov.make_short_sentence(300, tries=50))
+def generate_text(text):
+    markov = markovify.Text(text, state_size=1)
+    print(markov.make_short_sentence(300, tries=40))
+
+
+if __name__ == "__main__":
+    url = ""
+    if len(sys.argv) == 1:
+        subreddit = ""
+        while not subreddit:
+            subreddit = input("Generate post title from which subreddit? (Just the name, no /r/):\n")
+        url = requests.get("http://reddit.com/r/" + subreddit, headers=headers, timeout=10)
+    elif len(sys.argv) > 2:
+        print("Please only enter a single argument, the subreddit name!")
+        exit()
+    else:
+        url = requests.get("http://reddit.com/r/" + sys.argv[1], headers=headers, timeout=10)
+
+    generate_text(crawl_url(url))
